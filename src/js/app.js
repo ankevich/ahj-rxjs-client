bd82e99eee3495409c84bd39d101890b07cda3ae
Annie <overheads165@gmail.com>
2023-01-02 23:35:54 +0100

    получаем собщения

---
+++ src/js/app.js
@@ -0,0 +1,29 @@
+import { ajax } from "rxjs/ajax";
+import { map, catchError, of, repeat } from "rxjs";
+
+const messagesTable = document.querySelector("#messages");
+
+const messages$ = ajax.getJSON("http://localhost:3000/messages/unread").pipe(
+  repeat({ delay: 2000 }),
+  map((response) => response.messages),
+  catchError((error) => {
+    console.log("error: ", error);
+    return of(error);
+  })
+);
+
+function renderMessage(message) {
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>${message.from}</td>
+    <td>${message.body}</td>
+    <td>${message.received}</td>
+  `;
+  return row;
+}
+
+messages$.subscribe((messages) => {
+  messages
+    .map(renderMessage)
+    .forEach((row) => messagesTable.prepend(row));
+});
