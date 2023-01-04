b72b74bff98ff76e5a1dd569cc7cc43a8d7e8798
Annie <overheads165@gmail.com>
2023-01-04 14:58:53 +0100

    linter

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,27 @@
-import { ajax } from "rxjs/ajax";
-import { map, catchError, of, repeat } from "rxjs";
+import { ajax } from 'rxjs/ajax';
+import {
+  map, catchError, of, repeat,
+} from 'rxjs';
 
-const messagesTable = document.querySelector("#messages");
+const messagesTable = document.querySelector('#messages');
 
-const messages$ = ajax.getJSON("http://localhost:3000/messages/unread").pipe(
+const messages$ = ajax.getJSON('http://localhost:3000/messages/unread').pipe(
   repeat({ delay: 2000 }),
   map((response) => response.messages),
-  catchError((error) => {
-    console.log("error: ", error);
-    return of(error);
-  })
+  catchError((error) => of(error)),
 );
 
 function renderMessage(message) {
-  const row = document.createElement("tr");
-  const subject =
-    message.subject.length > 15
-      ? message.subject.slice(0, 15) + "..."
-      : message.subject;
-  const received = new Date(message.received).toLocaleString("ru-RU", {
-    hour: "2-digit",
-    minute: "2-digit",
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
+  const row = document.createElement('tr');
+  const subject = message.subject.length > 15
+    ? `${message.subject.slice(0, 15)}...`
+    : message.subject;
+  const received = new Date(message.received).toLocaleString('ru-RU', {
+    hour: '2-digit',
+    minute: '2-digit',
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
   });
   row.innerHTML = `
     <td>${message.from}</td>
